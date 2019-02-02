8913be712466726710d69ba8027e30707b5aa4ea
linhabc <jonathanrocrach2@gmail.com>
2019-02-02 22:12:32 +0700

    finished pagination

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,14 @@
 const db = require('../db.js');
 
 module.exports.index = (req, res)=>{
+	let page = parseInt(req.query.page) || 1;
+	let itemPerPage = 9;
+
+	let start = (page -1)*itemPerPage;
+	let end = page*itemPerPage;
+
 	res.render('product/index', {
-		products: db.get('products').value()
+		products: db.get('products').value().slice(start, end),
+		page: page
 	})
 };
